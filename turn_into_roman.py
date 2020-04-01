fc7db98eabea8da31c6db2071726d53019c3b355
King-Ny <62940350+Bot-queen@users.noreply.github.com>
2020-04-01 19:00:58 +0545

    Update turn_into_roman.py

--- turn_into_roman.py
+++ turn_into_roman.py
@@ -4,6 +4,7 @@ def int_to_roman(num):
     numerical = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1] 
     romans = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"] 
     i = 0
+    roman_num = ""
     while num > 0:
         for j in range(num // numerical[i]): 
             roman_num += romans[i] 
