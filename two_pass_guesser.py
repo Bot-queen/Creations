65be95694a338bd5bb503058383c47c3e88b6a33
King-Ny <62940350+Bot-queen@users.noreply.github.com>
2020-04-03 15:05:29 +0545

    Update and rename six_pass_guesser.py to two_pass_guesser.py

--- two_pass_guesser.py
+++ two_pass_guesser.py
@@ -9,14 +9,19 @@ pass_list = [
 ]
 
 def pass_guesser():
+    a = input("Enter password: ")
     while True:
         e = 0
+        for j in range(0, 30):
+            pass_list[j] = ""
         while e <= 29:
             for i in range(0, 2):
-                d = string.ascii_letters[random.randint(0, 51)] + string.digits[random.randint(0, 9)] + string.punctuation[random.randint(0, 31)]
+                d = string.ascii_letters[random.randint(0, 51)]
                 pass_list[e] = pass_list[e] + d
-            time.sleep(2)
-            print(pass_list[e])
+                if len(pass_list[e]) > 2:
+                    break
+            if pass_list[e] == a:
+                print("Password found! Your pass is", pass_list[e])
             e += 1
 
 pass_guesser()
